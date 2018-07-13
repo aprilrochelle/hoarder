fe0aa5814817e1f9fdcbb29c179e9cdf1bdb3c7a
April Nichols <nicholsar@gmail.com>
2018-07-12 23:01:16 -0500

    home route & component

--- src/app/App.js
+++ src/app/App.js
@@ -1,25 +1,38 @@
 import React, { Component } from 'react';
+import {Route, BrowserRouter, Redirect, Switch} from 'react-router-dom';
 import './App.css';
 
-import AllStuff from '../components/AllStuff/AllStuff';
-import Items from '../components/Items/Items';
-import Login from '../components/Login/Login';
-import MyStuff from '../components/MyStuff/MyStuff';
+// import AllStuff from '../components/AllStuff/AllStuff';
+import Home from '../components/Home/Home';
+// import Items from '../components/Items/Items';
+// import Login from '../components/Login/Login';
+// import MyStuff from '../components/MyStuff/MyStuff';
 import Navbar from '../components/Navbar/Navbar';
-import Register from '../components/Register/Register';
-import SingleItem from '../components/SingleItem/SingleItem';
+// import Register from '../components/Register/Register';
+// import SingleItem from '../components/SingleItem/SingleItem';
 
 class App extends Component {
   render () {
     return (
       <div className="App">
-        <AllStuff />
-        <Items />
-        <Login />
-        <MyStuff />
-        <Navbar />
-        <Register />
-        <SingleItem />
+        <BrowserRouter>
+          <div>
+            <Navbar />
+            <div className="container">
+              <div className="row">
+                <Switch>
+                  <Route path="/" exact component={Home}/>
+                  {/* <AllStuff />
+                  <Items />
+                  <Login />
+                  <MyStuff />
+                  <Register />
+                  <SingleItem /> */}
+                </Switch>
+              </div>
+            </div>
+          </div>
+        </BrowserRouter>
       </div>
     );
   }
